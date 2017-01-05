ccce80a3e1308d59c2fbd22e58dc9dfd6ca57c6d
BAN Jun <banjun@gmail.com>
2017-01-05 13:47:10 +0900

    warn code that may crash in runtime due to SR-3520

--- Pod/Classes/※ikemen.swift
+++ Pod/Classes/※ikemen.swift
@@ -16,3 +16,15 @@ public func ※<T>(value: T, modifier: (inout T) -> ()) -> T {
     modifier(&v)
     return v
 }
+
+// TODO: remove after https://bugs.swift.org/browse/SR-3520 is fixed
+@available(iOS, deprecated: 8, message: "can cause runtime crash. use type annotation. see SR-3520")
+@available(OSX, deprecated: 10.11, message: "can cause runtime crash. use type annotation. see SR-3520")
+public func ※<T: UnsafeSR3520>(value: T, modifier: (T) -> ()) -> T {
+    fatalError()
+}
+public protocol UnsafeSR3520 {}
+extension NSArray: UnsafeSR3520 {}
+extension NSString: UnsafeSR3520 {}
+extension Array: UnsafeSR3520 {}
+extension String: UnsafeSR3520 {}
